5cb2775f802f6c5ef10b1ecb4f4bf19ac4021543
AnkLowbrowJamison <jessevreynolds@gmail.com>
2023-05-17 22:15:22 -0600

    Move vite to dependencies

--- public/script.js
+++ public/script.js
@@ -48,7 +48,7 @@ function chatStripe(isAi, value, uniqueId) {
         <div class="wrapper ${isAi && 'ai'}">
             <div class="chat">
                 <div class="profile">
-                    <img src=${isAi ? bot : user} alt="${isAi ? 'bot' : 'user'}" />
+                    <img src=${isAi ? bot.src : user.src} alt="${isAi ? 'bot' : 'user'}" />
                 </div>
                 <div class="message" id=${uniqueId}>${value}</div>
             </div>
