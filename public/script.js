bff607668eae62be883f9d9ffec300f81d1a1aa5
AnkLowbrowJamison <jessevreynolds@gmail.com>
2023-05-17 21:56:03 -0600

    Initial commit

--- public/script.js
+++ public/script.js
@@ -49,7 +49,7 @@ function chatStripe(isAi, value, uniqueId) {
         <div class="wrapper ${isAi && 'ai'}">
             <div class="chat">
                 <div class="profile">
-                    <img src=${isAi ? bot : user} alt="${isAi ? 'bot' : 'user'}" />
+                    <img src=${isAi ? /assets/bot.svg : /assets/user.svg} alt="${isAi ? '/assets/bot.svg' : '/assets/user.svg'}" />
                 </div>
                 <div class="message" id=${uniqueId}>${value}</div>
             </div>
